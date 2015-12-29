7b165ce372521979bd08eab946cf987e1f119d94
Justin Durham <justin.durham@icloud.com>
2015-12-29 11:40:53 -0500

    Updated error handler

--- app.js
+++ app.js
@@ -6,5 +6,7 @@ logger.debug('debug test');
 logger.info('info test');
 logger.warn('warn test');
 logger.error('error test');
+logger.errorWithMessage('error message test', new Error('this is a test error'));
+logger.errorWithMessage('error message test', {test: 'test1'}, new Error('this is a test error'));
 
 process.exit();
