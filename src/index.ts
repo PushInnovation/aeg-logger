c17684f5b28265daf4f1095d55c592651adc50df
Entroper <entroper@gmail.com>
2017-07-13 10:04:26 -0400

    no namespaces

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import Logger from './logger';
+import { ILoggerConfig } from './logger';
+import * as winston from 'winston';
+import * as winstonError from 'winston-error';
+import * as config from 'config';
+
+const loggerConfig: ILoggerConfig = config.get('aeg-logger');
+
+winston.addColors({
+	debug: 'green',
+	info: 'green',
+	warn: 'yellow',
+	error: 'red'
+});
+
+const logger = new Logger(loggerConfig);
+
+winstonError(logger);
+
+export default logger;
--- src/logger.ts
+++ src/logger.ts
@@ -1,79 +1,58 @@
 import * as _ from 'lodash';
 import * as winston from 'winston';
 import { Papertrail } from 'winston-papertrail';
-import * as winstonError from 'winston-error';
-import * as config from 'config';
 
-export namespace aeg {
+export interface ILoggerConfig {
+	transports: any[];
+}
 
-	export interface ILoggerConfig {
-		transports: any[];
-	}
+export default class Logger extends winston.Logger {
 
-	export class Logger extends winston.Logger {
+	constructor (loggerConfig: ILoggerConfig) {
 
-		constructor (loggerConfig: ILoggerConfig) {
+		const transports: winston.TransportInstance[] = [];
 
-			const transports: winston.TransportInstance[] = [];
+		_.each(loggerConfig.transports, (transport) => {
 
-			_.each(loggerConfig.transports, (transport) => {
+			switch (transport.type) {
+				case 'console':
+					transports.push(new (winston.transports.Console)({
+						colorize: true,
+						level: transport.level,
+						handleExceptions: true
+					}));
+					break;
+				case 'file':
+					transports.push(new (winston.transports.File)({
+						colorize: true,
+						filename: transport.filename,
+						maxsize: 5242880,
+						maxFiles: 5,
+						tailable: true,
+						level: transport.level,
+						handleExceptions: true,
+						json: true
+					}));
+					break;
+				case 'paperTrail':
+					transports.push(new Papertrail({
+						host: transport.host,
+						port: transport.port,
+						level: transport.level,
+						program: transport.program,
+						colorize: false,
+						inlineMeta: true
+					}));
+					break;
+			}
 
-				switch (transport.type) {
-					case 'console':
-						transports.push(new (winston.transports.Console)({
-							colorize: true,
-							level: transport.level,
-							handleExceptions: true
-						}));
-						break;
-					case 'file':
-						transports.push(new (winston.transports.File)({
-							colorize: true,
-							filename: transport.filename,
-							maxsize: 5242880,
-							maxFiles: 5,
-							tailable: true,
-							level: transport.level,
-							handleExceptions: true,
-							json: true
-						}));
-						break;
-					case 'paperTrail':
-						transports.push(new Papertrail({
-							host: transport.host,
-							port: transport.port,
-							level: transport.level,
-							program: transport.program,
-							colorize: false,
-							inlineMeta: true
-						}));
-						break;
-				}
+		});
 
-			});
-
-			super({
-				transports,
-				exitOnError: false
-			});
-
-		}
+		super({
+			transports,
+			exitOnError: false
+		});
 
 	}
 
 }
-
-const loggerConfig: aeg.LoggerConfig = config.get('aeg-logger');
-
-winston.addColors({
-	debug: 'green',
-	info: 'green',
-	warn: 'yellow',
-	error: 'red'
-});
-
-const logger = new aeg.Logger(loggerConfig);
-
-winstonError(logger);
-
-export default logger;
