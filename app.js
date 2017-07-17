682a36a74b90b12212cd427446ae87a92385b30a
Entroper <entroper@gmail.com>
2017-07-17 16:36:12 -0400

    config

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const logger = require('./lib/logger').default;
+const logger = require('./lib/index').default;
 
 logger.debug('debug test');
 logger.info('info test');
 logger.warn('warn test');
 logger.error('error test');
-logger.errorWithMessage('error message test', new Error('this is a test error'));
-logger.errorWithMessage('error message test', {test: 'test1'}, new Error('this is a test error'));
+logger.error('error message test', new Error('this is a test error'));
+logger.error('error message test', {test: 'test1'}, new Error('this is a test error'));
 
 process.exit();
